731195eaa3de1cf94b9b8eb68677aae71c6954b9
Heiko Schaefer <heiko@rangun.de>
2022-06-01 07:54:18 +0200

    initial commit

---
+++ src/main/java/de/rangun/pangaeablocks/PangaeaBlocksPlugin.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2022 by Heiko Schäfer <heiko@rangun.de>
+ *
+ * This file is part of PangaeaBlocks.
+ *
+ * PangaeaBlocks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * PangaeaBlocks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with PangaeaBlocks.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.rangun.pangaeablocks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PangaeaBlocksPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+}
